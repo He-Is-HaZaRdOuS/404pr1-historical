b5a50c2024a0a01dcdbffba77d5cd91b5646e5a5
He-Is-HaZaRdOuS <wither725@gmail.com>
2024-02-13 22:58:38 +0300

    Timer.cpp: Hotfix: use uint64_t

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -11,11 +11,11 @@ Timer::~Timer() {
 void Timer::Stop() {
   m_EndTimepoint = std::chrono::high_resolution_clock::now();
 
-  const u_int64_t start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-  const u_int64_t end = std::chrono::time_point_cast<std::chrono::microseconds>(m_EndTimepoint).time_since_epoch().count();
+  const uint64_t start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+  const uint64_t end = std::chrono::time_point_cast<std::chrono::microseconds>(m_EndTimepoint).time_since_epoch().count();
 
-  const std::chrono::duration<long, std::ratio<1, 1000000>>::rep duration = end - start;
-  const double ms = duration * 0.001;
+  const std::chrono::duration<uint64_t, std::ratio<1, 1000000>>::rep duration = (end - start);
+  const auto ms = static_cast<double>(duration) * 0.001;
 
   std::cout << duration << "us (" << ms << "ms)" << std::endl;
 }
